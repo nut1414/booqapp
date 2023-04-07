e39192af82eb4e4e0478d6ac1c5a8b68712adb15
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2023-04-07 22:17:09 +0700

    first example

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  req.body
+  console.log(req.body)
+  res.status(200).json({ name: req.body })
 }
