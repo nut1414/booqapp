1487dc9d11c23314ae4f5e3d1960306a9323d4a5
bentorrrr <bentorrrrr@gmail.com>
2023-04-14 01:32:02 +0700

    Seperate Hashing and fix Register -> Hashing Pass

--- pages/api/register.js
+++ pages/api/register.js
@@ -11,13 +11,11 @@ export default async function register(req, res){
         if(!req.body?.UserName || !req.body?.PhoneNumber || !req.body?.Email || !req.body?.Password || !req.body?.FirstName || !req.body?.LastName) {
             return res.status(400).json({ message: 'All field must be filled.' })
         }
-        bcrypt.genSalt(saltround, function(err, salt) {
-            bcrypt.hash(req.body.Password, salt, function(err, hash) {
-                req.body.Password = hash
-            });
-        }, function(err) {
-            console.log(err)
-        });
+        const response = await fetch('http://localhost:3000/api/Hashing', { method: 'POST', body: JSON.stringify(req.body.Password)});
+        const hashedpassword = await response.json();
+        let { message } = hashedpassword;
+        let pw = message
+        console.log(pw)
         // Hash Password First Before Saving in The Database
         const User = await prisma.user.create({
                 data: {
@@ -29,7 +27,7 @@ export default async function register(req, res){
                     UserName: req.body.UserName,
                     PhoneNumber: req.body.PhoneNumber,
                     Email: req.body.Email,
-                    Password: req.body.Password,
+                    Password: pw,
                     FirstName: req.body.FirstName,
                     LastName: req.body.LastName,
                     JoinDate: new Date(),
