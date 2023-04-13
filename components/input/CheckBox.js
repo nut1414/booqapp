d86b3c67ef2caa995c9e07d642ecff82cf550bb6
bbasoo7 <nithikorn.bas@gmail.com>
2023-04-13 21:12:05 +0700

    initail commit

--- components/input/CheckBox.js
+++ components/input/CheckBox.js
@@ -2,11 +2,10 @@ export function CheckBox({ onChange, checked, defaultChecked, name, className })
   return (
     <>
     <form>
-    <input type = "checkbox" name="customher" class = " rounded-full text-gray-900 m-3 "></input> 
-    <label>Customer  </label> 
-    <br></br>
-    <input type = "checkbox" name="publisher" class = " rounded-full text-gray-900 m-3 "></input> 
-    <label>Publisher  </label> 
+    <p>
+    <input id={name} type = "checkbox" onChange={onChange} class = " rounded-full text-gray-900 m-3 "></input> 
+    <label for={name}>{name}</label> 
+    </p>
     </form>
     </>
   )
