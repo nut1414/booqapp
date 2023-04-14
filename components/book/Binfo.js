9923fd181efb9783a903bb7634d5650d119116ff
bbasoo7 <nithikorn.bas@gmail.com>
2023-04-14 17:41:18 +0700

    commit

--- components/book/Binfo.js
+++ components/book/Binfo.js
@@ -2,21 +2,21 @@
 
 import Link from "next/link";
 
-export function Binfo({picture,name,price,author,link,onclick}) {
+export function Binfo({picture,name,price,author,linkname,linkauthor,onclick}) {
     return (
       <div className="flex flex-col w-max">
      <div>
         <img  className ={"object-cover m-10 "} src= {"picture/"+picture} width="170" height="100" />
      </div>
      <div className="mb-2">
-        <Link href={link} className = " text-white hover:text-yellow-600 text-2xl w-max p-5 ">{name}</Link><br></br>
+        <Link href={linkname} className = " text-white hover:text-yellow-600 text-2xl w-max p-5 ">{name}</Link><br></br>
      </div>
      <div>
-        <Link href={link} className = "text-white hover:text-yellow-600 text-base w-max font-light underline p-5">{author}</Link>
+        <Link href={linkauthor} className = "text-white hover:text-yellow-600 text-base w-max font-light underline p-5">{author}</Link>
      </div>
      <div className="flex ">
      <p className = "text-white text-2xl  font-light p-5 object-center w-48">{price}</p>
-     <button className = {"rounded-3xl text-4xl "}>+</button>
+     <button className = {"rounded-3xl text-4xl "} onClick={onclick}>+</button>
      </div>
       </div>
     )
--- pages/books.js
+++ pages/books.js
@@ -4,7 +4,7 @@ import { Binfo } from "@/components/book/Binfo";
 export default function books() {
   return (
     <div>
-      <Binfo link={"https://www.youtube.com/watch?v=tGv7CUutzqU"} picture={"bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} picture={"bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
       
     </div>
   )
