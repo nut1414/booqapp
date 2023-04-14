16cecc67942473f376d9147b94ea2878a0fa002a
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2023-04-15 00:44:08 +0700

    book in template

--- components/book/Binfo.js
+++ components/book/Binfo.js
@@ -4,20 +4,20 @@ import Link from "next/link";
 
 export function Binfo({picture,name,price,author,linkname,linkauthor,onclick}) {
     return (
-      <div className="flex flex-col w-max">
-     <div>
-        <img  className ={"object-cover m-10 "} src= {"picture/"+picture} width="170" height="100" />
-     </div>
-     <div className="mb-2">
-        <Link href={linkname} className = " text-white hover:text-yellow-600 text-2xl w-max p-5 ">{name}</Link><br></br>
-     </div>
-     <div>
-        <Link href={linkauthor} className = "text-white hover:text-yellow-600 text-base w-max font-light underline p-5">{author}</Link>
-     </div>
-     <div className="flex ">
-     <p className = "text-white text-2xl  font-light p-5 object-center w-48">{price}</p>
-     <button className = {"rounded-3xl text-4xl hover:text-yellow-600 "} onClick={onclick}>+</button>
-     </div>
+    <div className="flex flex-col w-max">
+        <Link href={linkname}>
+          <img  className ={"object-cover m-10 "} src= {picture} width="170" height="100" />
+      </Link>
+      <div className="mb-2">
+          <Link href={linkname} className = " text-black  hover:text-spooky-orange transition-all text-2xl w-max p-5 ">{name}</Link><br></br>
       </div>
+      <div>
+          <Link href={linkauthor} className = "text-black  hover:text-spooky-orange transition-all text-base w-max font-light  p-5">{author}</Link>
+      </div>
+      <div className="flex">
+        <p className = "text-2xl  font-light p-5 object-center w-48">{price}</p>
+        <button className = {"rounded-3xl text-4xl hover:text-yellow-600 "} onClick={onclick}>+</button>
+      </div>
+    </div>
     )
   }
--- pages/books.js
+++ pages/books.js
@@ -1,11 +1,12 @@
 
 import { Binfo } from "@/components/book/Binfo";
+import { Template } from "@/components/common/Template";
 
 export default function books() {
   return (
-    <div>
-      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+    <Template>
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
       
-    </div>
+    </Template>
   )
 }
