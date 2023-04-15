848f81d92d18e077f2cebd8ba6ad9b69b4b0be0c
bbasoo7 <nithikorn.bas@gmail.com>
2023-04-15 16:16:30 +0700

    ทำ grid ให้แล้ว

--- pages/books.js
+++ pages/books.js
@@ -5,8 +5,28 @@ import { Template } from "@/components/common/Template";
 export default function books() {
   return (
     <Template>
+      <div className="grid grid-cols-5  grid-rows-4 gap-5">
       <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
-      
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/booktwo.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      <Binfo linkname={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} linkauthor={"https://www.youtube.com/watch?v=djV11Xbc914"} picture={"/picture/bookone.png"} name = {"Milo"} author={"กูไงจะใครล่ะ"} price ={250} />
+      </div>
     </Template>
   )
 }
