077f2c962ceab2033b846af524d7f7a1ba25f8db
bbasoo7 <nithikorn.bas@gmail.com>
2023-04-16 15:34:57 +0700

    เพิ่มตรงส่วนหน้าหนังสือหลัก แต่ยังไม่เสร็จ

--- components/input/Button.js
+++ components/input/Button.js
@@ -1,7 +1,7 @@
 export function Button({ onClick, text ,color}) {
   return (
     
-     <button className = {"w-28 h-10 m-8 rounded-3xl " + color} onClick={onClick}>{text}</button>
+     <button className = {"w-28 h-10 m-2  rounded-3xl " + color} onClick={onClick}>{text}</button>
 
   )
 }
--- pages/book/detail/[bookid].js
+++ pages/book/detail/[bookid].js
@@ -1,9 +1,22 @@
 import { Template } from "@/components/common/Template"
+import { Button } from "@/components/input/Button"
 
+const onClickHandle = () => {
+  console.log("test")
+} 
 export default function BookInfo() {
   return (
     <Template>
-      { /* Add your code here */ }
+      <div><img  className ={"object-cover m-10 float-left w-1/3 h-1/3 "} src= {"/picture/bookone.png"} /></div>
+        <div>
+          <div>
+        <h1 className="text-3xl font-bold">Milo</h1>
+        </div>
+        <div>
+       <Button color={"bg-[#FF8307]"} text={"Buy now"} onClick={onClickHandle} />
+       <Button color={"bg-[#FFFFFF] text-yellow-600 border-2 border-yellow-600"} text={"Add to cart"} onClick={onClickHandle}  />
+       </div>
+       </div>
     </Template>
   )
 }
