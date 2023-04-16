a65a780f96129fb3eb7584ad6783da8691d9b345
bbasoo7 <nithikorn.bas@gmail.com>
2023-04-16 16:13:31 +0700

    อัพเดท bookid

--- pages/book/detail/[bookid].js
+++ pages/book/detail/[bookid].js
@@ -7,15 +7,17 @@ const onClickHandle = () => {
 export default function BookInfo() {
   return (
     <Template>
-      <div><img  className ={"object-cover m-10 float-left w-1/3 h-1/3 "} src= {"/picture/bookone.png"} /></div>
-        <div>
+      <div><img  className ={"object-cover m-10 mr-16 float-left w-1/3 h-1/3 "} src= {"/picture/bookone.png"} /></div>
+        <div className="flex box-border">
+         <div className="block box-border w-full">
           <div>
-        <h1 className="text-3xl font-bold">Milo</h1>
-        </div>
-        <div>
-       <Button color={"bg-[#FF8307]"} text={"Buy now"} onClick={onClickHandle} />
-       <Button color={"bg-[#FFFFFF] text-yellow-600 border-2 border-yellow-600"} text={"Add to cart"} onClick={onClickHandle}  />
-       </div>
+            <p className="text-4xl font-bold mt-10 m-auto">    Milo</p>
+          </div>
+          <div>
+            <Button color={"bg-[#FF8307]"} text={"Buy now"} onClick={onClickHandle} />
+            <Button color={"bg-[#FFFFFF] text-yellow-600 border-2 border-yellow-600"} text={"Add to cart"} onClick={onClickHandle}  />
+          </div>
+         </div>
        </div>
     </Template>
   )
