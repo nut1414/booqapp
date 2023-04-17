56d73761d05d13404b97bb0a2ba2b02b4fbb1f7a
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2023-04-17 10:32:24 +0700

    banner in index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import { Template } from '@/components/common/Template'
+import { Banner } from '@/components/home/Banner'
 
 export default function Home() {
   return (
       <Template>
-        
+        <Banner/>
       </Template>
   )
 }
