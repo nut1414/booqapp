ac29d4aaf23ba595f3dedd2fe2d33236664feb3b
bbasoo7 <nithikorn.bas@gmail.com>
2023-05-02 18:49:09 +0700

    หน้า profile กับ เปลี่ยน password เสร็จแล้ว

---
+++ components/input/TextBoxInline.js
@@ -0,0 +1,9 @@
+export function TextBoxInline({ onChange, value, id, name, className ,type, label, readOnly, desorNot ,classNamelb ,classNamebox }) {
+
+    return (
+      <div> 
+        <label htmlFor={id ? id : name} className={"font-bold text-xl ml-3 " + classNamelb}>{label}  </label>
+        <input id={id ? id : name} name={name} type={type} onChange={onChange ? onChange : () => { }} value={value} className={" p-4 rounded-full text-gray-900 m-3 drop-shadow-sm border border-black w-96 h-10 "+classNamebox}></input>
+      </div>
+    )
+  }
--- components/user/ProfilePage.js
+++ components/user/ProfilePage.js
@@ -1,4 +1,8 @@
 import { useState } from "react"
+import { TextBoxLbInline } from "../input/TextBox"
+import Link from "next/link"
+import { TextBoxInline } from "../input/TextBoxInline"
+import { Button } from "../input/Button"
 
 export default function ProfilePage() {
   const [isChangingPassword, setIsChangingPassword] = useState(false)
@@ -8,11 +12,48 @@ export default function ProfilePage() {
       {
         isChangingPassword ? (
           <div>
-            <h1>Change Password Page</h1>
+          <div>
+              <div className="text-2xl font-bold mb-10">My Profile</div>
           </div>
+          <div>
+          <div>
+            <form className="ml-7">
+              <TextBoxInline classNamebox={"ml-28"} classNamelb={"mr-2"} label={"Old Password"} name={"Old Password"} type={"password"}></TextBoxInline>
+              <TextBoxInline classNamebox={"ml-28"} label={"New Passowrd"} name={"New Passowrd"} type={"password"}></TextBoxInline>
+              <TextBoxInline classNamebox={"ml-8"} label={"Confirm New password"} name={"Confirm New password"} type={"password"}></TextBoxInline>
+            </form>
+          </div>
+          <div className="float-right mt-10">
+          <Button type="submit" text={"Save"} />
+          </div>
+          </div>
+        </div>
         ) : (
           <div>
-            <h1>Profile Page</h1>
+            <div>
+                <div className="text-2xl font-bold mb-10">My Profile</div>
+            </div>
+            <div>
+              <div className="inline-flex">
+              <div className="text-xl font-bold mb-10 ml-10">Username</div>
+              <div className="text-xl  ml-9">TestUsername</div>
+              </div>  
+              <div className="mb-5">
+                <div className="text-xl font-bold ml-10 inline-flex">Password</div>
+                <Link className="text-black  hover:text-spooky-orange transition-all text-xl  underline w-max ml-10" href={"https://www.youtube.com/watch?v=5CKcSFBD9BI"}>Change</Link>
+              </div>
+            <div>
+              <form className="ml-7">
+                <TextBoxInline classNamebox={"ml-36"} classNamelb={"mr-1"} label={"Name"} name={"Name"} type={"text"}></TextBoxInline>
+                <TextBoxInline classNamebox={"ml-36"} label={"Phone"} name={"Phone"} type={"text"}></TextBoxInline>
+                <TextBoxInline classNamebox={"ml-36"} label={"E-mail"} name={"E-mail"} type={"text"}></TextBoxInline>
+                <TextBoxInline classNamebox={"ml-8"} label={"Confirm password"} name={"Confirm password"} type={"password"}></TextBoxInline>
+              </form>
+            </div>
+            <div className="float-right mt-10">
+            <Button type="submit" text={"Save"} />
+            </div>
+            </div>
           </div>
           )
       }
