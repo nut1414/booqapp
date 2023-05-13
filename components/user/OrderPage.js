635cfd6905568b08d44eb650c5f4a800c9f0e54b
bbasoo7 <nithikorn.bas@gmail.com>
2023-05-14 01:24:18 +0700

    OrderPage เสร็จแล้ว เหลือปุ่มกดแสดงแค่statusนั้นๆ

--- components/user/OrderPage.js
+++ components/user/OrderPage.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Order } from "../order/Order"
+import { Button } from "../input/Button"
 
 export default function OrderPage() {
   const filter = ['all', 'pay', 'waitship', 'ship', 'complete']
@@ -7,8 +8,17 @@ export default function OrderPage() {
   
   return (
     <div>
-     <Order status={"shipping"}></Order>
+      <div className="grid grid-cols-5 mb-10 ">
+        <button className=" border-b p-3 border-b-slate-950 hover:border-spooky-orange hover:text-spooky-orange font-bold" type="button">All</button>
+        <button className=" border-b p-3 border-b-slate-950 hover:border-spooky-orange hover:text-spooky-orange font-bold" type="button">To Pay</button>
+        <button className=" border-b p-3 border-b-slate-950 hover:border-spooky-orange hover:text-spooky-orange font-bold" type="button">To Ship</button>
+        <button className=" border-b p-3 border-b-slate-950 hover:border-spooky-orange hover:text-spooky-orange font-bold" type="button">To Receive</button>
+        <button className=" border-b p-3 border-b-slate-950 hover:border-spooky-orange hover:text-spooky-orange font-bold" type="button">Complete</button>
+      </div>
+     <div>
+     <Order className="hidden" status={"shipping"}></Order>
       <Order status={"unpaid"}></Order>
+      </div>
     </div>
   )
 }
