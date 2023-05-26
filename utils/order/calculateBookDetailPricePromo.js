9408e8addcc6e13269238e379b2a7361b839cb4f
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2023-05-26 07:55:03 +0700

    add calculate current promotion

---
+++ utils/order/calculateBookDetailPricePromo.js
@@ -0,0 +1,13 @@
+export default function calculateBookDetailPricePromo(bookdetail) {
+  let promotionbook = Array.isArray(bookdetail?.promotionbook)
+    ? bookdetail?.promotionbook
+    : [];
+  const maxdiscountpromo = promotionbook.reduce((acc, curr) => {
+    return Math.max(acc.promotion.DiscountPercent, curr.promotion.DiscountPercent) >= acc.promotion.DiscountPercent ? curr: acc;
+  }
+    , { promotion: { DiscountPercent: 0 } })
+  console.log("maxdiscountpromo", maxdiscountpromo)
+  bookdetail.CurrentPromotion = maxdiscountpromo.promotion.DiscountPercent > 0 ? maxdiscountpromo.promotion : null;
+  bookdetail.FinalPrice =  +parseFloat(bookdetail.Price - (bookdetail.Price * maxdiscountpromo.promotion.DiscountPercent / 100)).toFixed(2);
+  return bookdetail;
+}
--- utils/order/calculateOrderTotalDiscountShip.js
+++ utils/order/calculateOrderTotalDiscountShip.js
@@ -5,16 +5,31 @@ export default function calculateOrderTotalDiscountShip(orderGroup, items='items
     let totalWeight = 0
     let totalPrice = 0
     group[items].forEach((item, index, arr) => {
-      let alldiscount = []
-      if (Array.isArray(item.book[promotionbook])) {
-        alldiscount = item.book[promotionbook]?.map((pro) => pro.promotion.DiscountPercent) || [0]
-      } else {
-        alldiscount = [item.book[promotionbook]?.promotion?.DiscountPercent ? item.book[promotionbook]?.promotion?.DiscountPercent : 0]
-      }
-      let maxdiscount = alldiscount.length ? Math.max(...alldiscount) : 0
-      console.log("alldiscount",alldiscount )
-      console.log("maxdiscount",maxdiscount)
-      arr[index].book.FinalPrice = item.book.Price - (item.book.Price * maxdiscount / 100)
+      // let alldiscount = []
+      // if (Array.isArray(item.book[promotionbook])) {
+      //   alldiscount = item.book[promotionbook]?.map((pro) => pro.promotion.DiscountPercent) || [0]
+      // } else {
+      //   alldiscount = [item.book[promotionbook]?.promotion?.DiscountPercent ? item.book[promotionbook]?.promotion?.DiscountPercent : 0]
+      // }
+      const maxdiscountpromo = item.book[promotionbook]?.reduce(
+        (acc, curr) => {
+          return Math.max(
+            acc.promotion.DiscountPercent,
+            curr.promotion.DiscountPercent
+          ) >= acc.promotion.DiscountPercent
+            ? curr
+            : acc;
+        },
+        { promotion: { DiscountPercent: 0 } }
+      );
+      // let maxdiscount = alldiscount.length ? Math.max(...alldiscount) : 0
+      // console.log("alldiscount",alldiscount )
+      // console.log("maxdiscount",maxdiscount)
+      arr[index].book.CurrentPromotion = maxdiscountpromo.promotion.DiscountPercent > 0 ? maxdiscountpromo.promotion : null;
+      arr[index].book.FinalPrice = +parseFloat(
+        item.book.Price - (item.book.Price * maxdiscountpromo.promotion.DiscountPercent) / 100,
+        2
+      );
       totalWeight += item.book.Weight * item.Quantity
       totalPrice += (item.book.FinalPrice ) * item.Quantity 
     })
