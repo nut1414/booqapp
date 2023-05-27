10d250554285db599291ae422498a67abb79987b
bbasoo7 <nithikorn.bas@gmail.com>
2023-05-27 16:33:33 +0700

    หน้า paymentdetail เสร็จแล้ว

--- pages/paymentdetail.js
+++ pages/paymentdetail.js
@@ -1,12 +1,17 @@
 import { Template } from "@/components/common/Template";
 import { Button } from "@/components/input/Button";
 import { useRouter } from "next/router"
+import { PaymentDetail1 } from "@/components/paymentDetail/PaymentDetail1";
+import { PaymentDetail2 } from "@/components/paymentDetail/PaymentDetail2";
 export default function paymentconfirm(orderid) {
  // verification drop down has 4 value to pick from -> all, unverified, pending, verified
  const router= useRouter()
   return (
     <Template>
-      <div className="bg-black h-80 w-24"></div>
+      <div className="inline-flex">
+      <PaymentDetail1></PaymentDetail1>
+      <div className=" ml-20"><PaymentDetail2 ordernumber={"BK0000001"}></PaymentDetail2></div>
+      </div>
     </Template>
   )
 }
