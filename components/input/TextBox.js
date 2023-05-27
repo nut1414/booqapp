5dadf0dc2bc02097dd86eeb58fd4061ff3071078
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2023-05-28 00:14:58 +0700

    fix broken textbox

--- components/input/TextBox.js
+++ components/input/TextBox.js
@@ -4,7 +4,7 @@ export function TextBox({ onChange, value, id, name, className ,type, label, rea
   return (
     <div> 
       <label htmlFor={id ? id : name} className="m-3 font-bold">{label}  </label><br></br>
-      <input id={id ? id : name} name={name} type={type} onChange={onChange ? onChange : () => { }} value={value} className={" p-4 rounded-full text-gray-900 m-3 drop-shadow-sm border border-black  "+ noWidth ? " " : "w-96" + labelclass}></input>
+      <input id={id ? id : name} name={name} type={type} onChange={onChange ? onChange : () => { }} value={value} className={" p-4 rounded-full text-gray-900 m-3 drop-shadow-sm border border-black  w-96 " + labelclass}></input>
     </div>
   )
 }
