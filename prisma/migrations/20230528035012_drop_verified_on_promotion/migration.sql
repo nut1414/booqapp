ab5124c1a7dbbf066855aa51fd89a312342c64c7
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2023-05-28 10:50:31 +0700

    drop verified on promotion

---
+++ prisma/migrations/20230528035012_drop_verified_on_promotion/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `Verified` on the `promotion` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `promotion` DROP COLUMN `Verified`;
